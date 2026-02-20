3e6006c5a19f7e127eaa887e43bb2cee3b4953ff
Bunyod <shbunyod2197@gmail.com>
2026-02-20 16:18:26 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+# 1
+def takror(a):
+    print(f"Salom: {a}")
+
+takror("bunod")
+# 2
+def raqam(a):
+    print(f"son: {a}")
+
+raqam(5)
+# 3
+def qosh(a, c):
+    print(a + c)
+
+qosh(3, 8)
+# 4
+def ism_yosh(a, b):
+    print(f"salom: {a}, yosh: {b}")
+
+ism_yosh("bunyod", 15)
+# 5
+def kvadrast(a):
+    print(a * a)
+kvadrast(5)
+# 6
+def matn(a, b):
+    print(a * b)
+
+matn("salom", 8)
+# 7
+def r(a, b):
+    print(a -b)
+
+r(45, 83)
+# 8
+def ism(a):
+    print(f"Hurmatli: {a}")
+
+ism("Abanet")
+# 9
+def kop(a, b):
+    print(a * b)
+
+kop(3, 8)
+10
+def kop(a, b):
+    print(a / b)
+
+kop(3, 8)
